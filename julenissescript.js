67f2fc55ef166041635fc7e2c18f7d67ceee0805
John Doe <johndoe@example.com>
2023-12-19 09:40:38 +0100

    ferdig

--- julenissescript.js
+++ julenissescript.js
@@ -1,14 +1,36 @@
 
-
+const seOnskeEl = document.querySelector("#seOnske")
 const onskeDivEl = document.querySelector("#onskeDiv")
 
 const json = JSON.parse(localStorage.getItem("julenisseJSON"))
+
+
+seOnskeEl.onclick = () => {
+
+    document.body.id = ""
+    seOnskeEl.style.display = "none"
+    seOnskeEl.innerHTML = ""
+
+}
+
 
 const makeOnske = (onske, i) => {
 
     const boxEl = document.createElement("div")
 
-    boxEl.className = ""
+    boxEl.className = "pointer"
+
+    boxEl.onclick = () => {
+
+        seOnskeEl.innerHTML = ""
+
+        document.body.id = "faded"
+        seOnskeEl.style.display = "flex"
+
+        seOnskeEl.innerHTML += `<h2>Ønske nummer ${i + 1}</h2>`
+        seOnskeEl.innerHTML += `<p>Type: ${onske.type}</p>`
+        seOnskeEl.innerHTML += `<p>Onske: ${onske.onske}</p>`
+    }
 
     boxEl.innerHTML += `<h2>Ønske nummer ${i + 1}</h2>`
     boxEl.innerHTML += `<p>Type: ${onske.type}</p>`
